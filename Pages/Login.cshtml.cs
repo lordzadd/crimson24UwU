98a8a26aba9e5aa5adb35d45b2bb15682f9bc181
ALangland <125757998+ALangland@users.noreply.github.com>
2024-02-18 07:35:35 -0800

    Moved some things around

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,6 +1,6 @@
 
-using Microsoft.AspNetCore.Authentication;
 using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Auth0_Blazor.Pages
